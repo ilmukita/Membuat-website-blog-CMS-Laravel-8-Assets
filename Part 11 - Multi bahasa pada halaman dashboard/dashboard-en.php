b15d0b185a5ab0ec716ade28569e7c5c4da5242d
FauziAris <aarisfauji24@gmail.com>
2021-04-10 00:10:01 +0700

    Part 11

---
+++ Part 11 - Multi bahasa pada halaman dashboard/dashboard-en.php
@@ -0,0 +1,31 @@
+<?php
+/*
+language : English
+*/
+return [
+    'title' => [
+        'index' => "Dashboard",
+    ],
+    'greeting' => [
+        'welcome' => "Welcome :name"
+    ],
+    'menu' => [
+        'master' => 'Master',
+        'user_permission' => 'User permission',
+        'setting' => 'Setting',
+    ],
+    'link' => [
+        'dashboard' => 'Dashboard',
+        'posts' => 'Posts',
+        'categories' => 'Categories',
+        'tags' => 'Tags',
+        'file_manager' => 'File Manager',
+        'roles' => 'Roles',
+        'users' => 'Users',
+        'profile' => 'Profile',
+        'logout' => 'Logout',
+    ],
+    'label' => [
+        'logged_in_as' => 'Logged in as',
+    ]
+];
---
+++ Part 11 - Multi bahasa pada halaman dashboard/dashboard-id.php
@@ -0,0 +1,31 @@
+<?php
+/*
+language : English
+*/
+return [
+    'title' => [
+        'index' => "Dashbor",
+    ],
+    'greeting' => [
+        'welcome' => "Selamat datang :name"
+    ],
+    'menu' => [
+        'master' => 'Master',
+        'user_permission' => 'Izin pengguna',
+        'setting' => 'Pengaturan',
+    ],
+    'link' => [
+        'dashboard' => 'Dasbor',
+        'posts' => 'Posting',
+        'categories' => 'Kategori',
+        'file_manager' => 'Manajemen file',
+        'roles' => 'Wewenang',
+        'users' => 'Pengguna',
+        'profile' => 'Profil',
+        'logout' => 'Logout',
+
+    ],
+    'label' => [
+        'logged_in_as' => 'Masuk sebagai',
+    ]
+];
