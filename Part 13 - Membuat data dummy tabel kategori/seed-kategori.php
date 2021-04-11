84010bfbdb5c0dc839fcf46062d1ca3cb60005fc
FauziAris <aarisfauji24@gmail.com>
2021-04-11 11:32:17 +0700

    Create seed-kategori.php

---
+++ Part 13 - Membuat data dummy tabel kategori/seed-kategori.php
@@ -0,0 +1,56 @@
+[
+[
+'title' => 'HTML',
+'slug' => 'html',
+'description' => 'HTML adalah singkatan dari Hypertext Markup Language',
+'thumbnail' => 'noimage.jpg',
+'created_at' => date('Y-m-d H:i:s'),
+'updated_at' => date('Y-m-d H:i:s'),
+'parent_id' => null
+],
+[
+'title' => 'HTML basic',
+'slug' => 'html-basic-1',
+'description' => 'HTML tingkat dasar',
+'thumbnail' => 'noimage.jpg',
+'created_at' => date('Y-m-d H:i:s'),
+'updated_at' => date('Y-m-d H:i:s'),
+'parent_id' => 1
+],
+[
+'title' => 'HTML advanced',
+'slug' => 'html-advanced-1',
+'description' => 'HTML tingkat dasar',
+'thumbnail' => 'noimage.jpg',
+'created_at' => date('Y-m-d H:i:s'),
+'updated_at' => date('Y-m-d H:i:s'),
+'parent_id' => 1
+],
+[
+'title' => 'CSS',
+'slug' => 'css',
+'description' => 'CSS atau Cascading Style Sheets adalah salah satu topik yang harus diketahui dalam pengembangan website.',
+'thumbnail' => 'noimage.jpg',
+'created_at' => date('Y-m-d H:i:s'),
+'updated_at' => date('Y-m-d H:i:s'),
+'parent_id' => null
+],
+[
+'title' => 'Javascript',
+'slug' => 'javascript',
+'description' => 'JavaScript adalah salah satu bahasa pemrograman yang sering digunakan oleh website programmer atau website developer.',
+'thumbnail' => 'noimage.jpg',
+'created_at' => date('Y-m-d H:i:s'),
+'updated_at' => date('Y-m-d H:i:s'),
+'parent_id' => null
+],
+[
+'title' => 'PHP',
+'slug' => 'php',
+'description' => 'Hypertext Preprocessor adalah bahasa skrip yang dapat ditanamkan atau disisipkan ke dalam HTML. PHP banyak dipakai untuk memprogram situs web dinamis. PHP dapat digunakan untuk membangun sebuah CMS.',
+'thumbnail' => 'noimage.jpg',
+'created_at' => date('Y-m-d H:i:s'),
+'updated_at' => date('Y-m-d H:i:s'),
+'parent_id' => null
+],
+]
