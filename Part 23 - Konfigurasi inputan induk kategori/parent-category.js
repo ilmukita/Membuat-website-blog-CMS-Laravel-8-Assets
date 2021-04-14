d867d1c1341c065b6a0ae505e9633b31c302e769
FauziAris <aarisfauji24@gmail.com>
2021-04-15 00:37:45 +0700

    Part 23

---
+++ Part 23 - Konfigurasi inputan induk kategori/parent-category.js
@@ -0,0 +1,21 @@
+//parent category
+$('#id').select2({
+   theme: 'bootstrap4',
+   language: "",
+   allowClear: true,
+   ajax: {
+      url: "",
+      dataType: 'json',
+      delay: 250,
+      processResults: function(data) {
+         return {
+            results: $.map(data, function(item) {
+               return {
+                  text: item.title,
+                  id: item.id
+               }
+            })
+         };
+      }
+   }
+});
