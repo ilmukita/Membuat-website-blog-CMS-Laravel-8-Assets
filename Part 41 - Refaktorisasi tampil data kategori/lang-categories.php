fdc1d040e96922d7d61df764354238f74d614e27
FauziAris <aarisfauji24@gmail.com>
2021-05-02 02:10:08 +0700

    Create lang-categories.php

---
+++ Part 41 - Refaktorisasi tampil data kategori/lang-categories.php
@@ -0,0 +1,17 @@
+<?php
+
+//EN
+'label' => [
+	'no_data' => [
+		'fetch' => "No category data yet",
+		'search' => ":keyword category not found",
+	]
+]
+//ID
+
+'label' => [
+	'no_data' => [
+		'fetch' => "Data kategori belum ada",
+		'search' => "Kategori :keyword tidak ditemukan",
+	]
+]
