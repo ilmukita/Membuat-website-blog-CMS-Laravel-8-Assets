73057fb82b4247a93d35a159d963f8e271a880e0
FauziAris <aarisfauji24@gmail.com>
2021-05-30 01:50:25 +0700

    Part 76

---
+++ Part 76 - Membuat halaman utama file manager/filemanager-en.php
@@ -0,0 +1,24 @@
+<?php
+/*
+language : English
+*/
+return [
+    'title' => [
+        'index' => 'File manager'
+    ],
+    'form_control' => [
+        'select' => [
+            'type' => [
+                'option' => [
+                    'image' => 'Image',
+                    'file' => 'File',
+                ]
+            ]
+        ]
+    ],
+    'button' => [
+        'apply' => [
+            'value' => 'Apply'
+        ]
+    ]
+];
---
+++ Part 76 - Membuat halaman utama file manager/filemanager-id.php
@@ -0,0 +1,24 @@
+<?php
+/*
+language : Indonesia
+*/
+return [
+    'title' => [
+        'index' => 'Manajer file'
+    ],
+    'form_control' => [
+        'select' => [
+            'type' => [
+                'option' => [
+                    'image' => 'Gambar',
+                    'file' => 'Berkas',
+                ]
+            ]
+        ]
+    ],
+    'button' => [
+        'apply' => [
+            'value' => 'Terapkan'
+        ]
+    ]
+];
