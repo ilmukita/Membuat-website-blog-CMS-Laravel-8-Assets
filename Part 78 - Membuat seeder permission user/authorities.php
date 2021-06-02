032f507dc269b8c3f2a7ccd437052f99225e0cfd
FauziAris <aarisfauji24@gmail.com>
2021-06-02 08:48:18 +0700

    Part 78

---
+++ Part 78 - Membuat seeder permission user/authorities.php
@@ -0,0 +1,36 @@
+'authorities' => [
+        'manage_posts' => [
+            'post_show',
+            'post_create',
+            'post_update',
+            'post_detail',
+            'post_delete'
+        ],
+        'manage_categories' => [
+            'category_show',
+            'category_create',
+            'category_update',
+            'category_detail',
+            'category_delete'
+        ],
+        'manage_tags' => [
+            'tag_show',
+            'tag_create',
+            'tag_update',
+            'tag_delete'
+        ],
+        'manage_roles' => [
+            'role_show',
+            'role_create',
+            'role_update',
+            'role_detail',
+            'role_delete'
+        ],
+        'manage_users' => [
+            'user_show',
+            'user_create',
+            'user_update',
+            'user_detail',
+            'user_delete'
+        ],
+    ],
