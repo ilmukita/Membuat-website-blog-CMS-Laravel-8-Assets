bfb5a9487ca8c4fd3b2c69f272849f227c4c3d36
FauziAris <aarisfauji24@gmail.com>
2021-07-01 21:20:53 +0700

    Part 92

---
+++ Part 92 - Membuat halaman utama user/users-en.php
@@ -0,0 +1,94 @@
+<?php
+/*
+language : English
+*/
+return [
+    'title' => [
+        'index' => 'Users',
+        'create' => 'Add user',
+        'edit' => 'Edit user',
+    ],
+    'form_control' => [
+        'input' => [
+            'name' => [
+                'label' => 'Name',
+                'placeholder' => 'Enter name',
+                'attribute' => 'name'
+            ],
+            'email' => [
+                'label' => 'Email',
+                'placeholder' => 'Enter email',
+                'attribute' => 'email'
+            ],
+            'password' => [
+                'label' => 'Password',
+                'placeholder' => 'Enter password',
+                'attribute' => 'password'
+            ],
+            'password_confirmation' => [
+                'label' => 'Password confirmation',
+                'placeholder' => 'Retype password',
+                'attribute' => 'password confirmation'
+            ],
+            'search' => [
+                'label' => 'Search',
+                'placeholder' => 'Search for users',
+                'attribute' => 'search'
+            ]
+        ],
+        'select' => [
+            'role' => [
+                'label' => 'Role',
+                'placeholder' => 'Choose role',
+                'attribute' => 'role'
+            ]
+        ]
+    ],
+    'label' => [
+        'name' => 'Name',
+        'email' => 'Email',
+        'role' => 'Role',
+        'no_data' => 'User data does not exist yet',
+    ],
+    'button' => [
+        'create' => [
+            'value' => 'Add'
+        ],
+        'save' => [
+            'value' => 'Save'
+        ],
+        'edit' => [
+            'value' => 'Edit'
+        ],
+        'delete' => [
+            'value' => 'Delete'
+        ],
+        'cancel' => [
+            'value' => 'Cancel'
+        ],
+    ],
+    'alert' => [
+        'create' => [
+            'title' => 'Add user',
+            'message' => [
+                'success' => "User saved successfully.",
+                'error' => "An error occurred while saving the user. :error"
+            ]
+        ],
+        'update' => [
+            'title' => 'Edit user',
+            'message' => [
+                'success' => "User updated successfully.",
+                'error' => "An error occurred while updating the user. :error"
+            ]
+        ],
+        'delete' => [
+            'title' => 'Delete user',
+            'message' => [
+                'confirm' => "Are you sure you want to delete :name users?",
+                'success' => "User deleted successfully.",
+                'error' => "An error occurred while deleting the user. :error"
+            ]
+        ],
+    ]
+];
---
+++ Part 92 - Membuat halaman utama user/users-id.php
@@ -0,0 +1,94 @@
+<?php
+/*
+language : Indonesia
+*/
+return [
+    'title' => [
+        'index' => 'User',
+        'create' => 'Tambah user',
+        'edit' => 'Ubah user',
+    ],
+    'form_control' => [
+        'input' => [
+            'name' => [
+                'label' => 'Nama',
+                'placeholder' => 'Masukan nama',
+                'attribute' => 'nama'
+            ],
+            'email' => [
+                'label' => 'Email',
+                'placeholder' => 'Masukan email',
+                'attribute' => 'email'
+            ],
+            'password' => [
+                'label' => 'Password',
+                'placeholder' => 'Masukan password',
+                'attribute' => 'password'
+            ],
+            'password_confirmation' => [
+                'label' => 'Konfirmasi password',
+                'placeholder' => 'Ketik ulang password',
+                'attribute' => 'konfirmasi password'
+            ],
+            'search' => [
+                'label' => 'Cari',
+                'placeholder' => 'Cari pengguna',
+                'attribute' => 'cari'
+            ]
+        ],
+        'select' => [
+            'role' => [
+                'label' => 'Wewenang',
+                'placeholder' => 'Pilih wewenang',
+                'attribute' => 'wewenang'
+            ]
+        ]
+    ],
+    'label' => [
+        'name' => 'Nama',
+        'email' => 'Email',
+        'role' => 'Wewenang',
+        'no_data' => 'Data pengguna belum ada',
+    ],
+    'button' => [
+        'create' => [
+            'value' => 'Tambah'
+        ],
+        'save' => [
+            'value' => 'Simpan'
+        ],
+        'edit' => [
+            'value' => 'Ubah'
+        ],
+        'delete' => [
+            'value' => 'Hapus'
+        ],
+        'cancel' => [
+            'value' => 'Batal'
+        ],
+    ],
+    'alert' => [
+        'create' => [
+            'title' => "Tambah user",
+            'message' => [
+                'success' => "User berhasil disimpan.",
+                'error' => "Terjadi kesalahan saat menyimpan user. :error"
+            ]
+        ],
+        'update' => [
+            'title' => 'Ubah user',
+            'message' => [
+                'success' => "User berhasil diperbaharui.",
+                'error' => "Terjadi kesalahan saat perbarui user. :error"
+            ]
+        ],
+        'delete' => [
+            'title' => 'Hapus user',
+            'message' => [
+                'confirm' => "Yakin akan menghapus user :title ?",
+                'success' => "User berhasil dihapus",
+                'error' => "Terjadi kesalahan saat menghapus user. :error"
+            ]
+        ],
+    ]
+];
