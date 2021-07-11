c8d991ca009e324023fadf3668638f728bae41e0
FauziAris <aarisfauji24@gmail.com>
2021-07-12 00:33:30 +0700

    Part 106

---
+++ Part 106 - Validasi hapus role user/lang-roles.php
@@ -0,0 +1,7 @@
+<?php
+
+//[en] roles.alert.delete.message.warning
+'warning' => "Sorry, the :name role cannot be deleted. Because it's still in use.",
+
+//[id] roles.alert.delete.message.warning
+'warning' => "Maaf, wewenang :name belum dapat dihapus. Karena masih digunakan.",
